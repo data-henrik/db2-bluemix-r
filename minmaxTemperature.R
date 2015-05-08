3bc7fd51b6df7409efdd180f12bd9d6025a66eed
Henrik Loeser <data-henrik@users.noreply.github.com>
2015-05-08 11:15:26 +0200

    Update minmaxTemperature.R
    Adapted to latest R package for dashDB and DB2 (ibmdbR)

--- minmaxTemperature.R
+++ minmaxTemperature.R
@@ -4,16 +4,16 @@
 ## For plotting, we are using ggplot2 package
 ## 
 library(ggplot2)
-library(bluR)
+library(ibmdbR)
 
 ## initialize DB2 connection and environment
-con <- bluConnect("BLUDB","","")
-bluAnalyticsInit(con)
+con <- idaConnect("BLUDB","","")
+idaInit(con)
 
 ## query DB2 weather data and fetch min/max values of min/max values
 ## (lower/upper boundary each) 
 query<-paste('select max(lufttemperatur_maximum) as maxmax,min(lufttemperatur_minimum) as minmin,min(lufttemperatur_maximum) as minmax,max(lufttemperatur_minimum) as maxmin,tag from (select lufttemperatur_maximum, lufttemperatur_minimum, day(mdatum) as tag from blu01023.klima where month(mdatum)=9) group by tag order by tag asc') 
-df <- bluQuery(query,as.is=F)
+df <- idaQuery(query,as.is=F)
 
 ## Some plotting needs to be done
 jpeg(type='cairo',"tempe.jpg",width=800,height=600) 
@@ -28,5 +28,5 @@ ggplot(df, aes(x = TAG))+ylab("Temperature")+xlab("Day")+
 
 sink('/dev/null') 
 
-bluClose(con)
+idaClose(con)
 ## connection is closed, we are done
